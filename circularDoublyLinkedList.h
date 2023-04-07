caae916809162b2e44c648d82f4324425316257d
Ahmed yehia <yehiaahmed900@gmail.com>
2023-04-07 15:16:30 +0200

    Implemented Circular Double Linked List

---
+++ circularDoublyLinkedList.h
@@ -0,0 +1,69 @@
+#ifndef DS_ASSIGN2_CIRCULARDOUBLYLINKEDLIST_H
+#define DS_ASSIGN2_CIRCULARDOUBLYLINKEDLIST_H
+
+#include <iostream>
+using namespace std;
+
+///@Author: Ahmed Yehia
+template <class T>
+struct node {
+    T data;
+    node *next;
+    node *prev;
+    node(){
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+template <class T>
+class circularDoublyLinkedList {
+private:
+    node<T> *head;
+    node<T> *tail;
+    int size;
+public:
+    circularDoublyLinkedList(){
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    }
+    int getSize(){
+        return size;
+    }
+    void insertAtHead(T element);
+    void insertAtTail(T element);
+    void insertAt(int index , T element);
+    void deleteAtHead();
+    void deleteAtTail();
+    void deleteAt(int index);
+    T retrieveAt( int index);
+    void replaceAt(int index, T element);
+    bool isExist(T element);
+    bool isItemAtEqual(int index, T element);
+    void swap(int index1, int index2);
+    bool isEmpty();
+    void clear();
+
+
+
+
+
+
+    void printList(){
+        node<T> *temp = head;
+        bool start = true;
+        while(temp!=head || start){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+            start = false;
+        }
+        cout<<endl;
+    }
+
+
+};
+
+
+
+
+#endif //DS_ASSIGN2_CIRCULARDOUBLYLINKEDLIST_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "circularDoublyLinkedList.cpp"
 using namespace std;
 
 int main(){
-    cout<<"Hello World";
+    circularDoublyLinkedList<int> list;
+    list.insertAtTail(1);
+    list.insertAtTail(2);
+    list.insertAtTail(3);
+    list.insertAtTail(4);
+    list.printList();
+    list.insertAt(2,5);
+    list.printList();
+    list.deleteAt(2);
+    list.printList();
+    list.insertAt(4,2);
+    list.printList();
+    cout<<list.getSize()<<endl;
+    list.deleteAt(4);
+    list.printList();
+    cout<<list.getSize()<<endl;
+    list.replaceAt(2,10);
+    list.printList();
+    list.clear();
+    list.printList();
+
+
 }
