96c227f97e1dc8020292ae05f6eb30ed1196144f
Ahmed yehia <yehiaahmed900@gmail.com>
2023-04-29 12:15:45 +0200

    refactoring

---
+++ Queue_Problems/Queue_prob1.cpp
@@ -0,0 +1,19 @@
+// Author : Ahmed Yehia
+#include <iostream>
+#include "queue.cpp"
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    Queue<string> q;
+    q.enqueue("1");
+    string binary = "";
+    for(int i=0;i<n;i++){
+        binary = q.dequeue();
+        cout<<binary<<' ';
+        q.enqueue(binary+"0");
+        q.enqueue(binary+"1");
+    }
+    cout<<endl;
+
+}
---
+++ Queue_Problems/Queue_prob3.cpp
@@ -0,0 +1,50 @@
+#include "queue.cpp"
+using namespace std;
+//Author : Ahmed Yehia
+void sortQueue(Queue<int> &q){
+ if(q.isEmpty())
+    return;
+ Queue<int> temp_q;
+    while(!q.isEmpty()) {
+        int curr_min = q.first();
+        int curr_size = q.getSize();
+        int i = 0;
+        while(i < curr_size) {
+            if(q.first() < curr_min) {
+                curr_min = q.first();
+            }
+            q.enqueue(q.first());
+            q.dequeue();
+
+            i++;
+        }
+
+        i = 0;
+        while(i < curr_size) {
+            if(q.first() != curr_min) {
+                q.enqueue(q.first());
+            }
+            else {
+                temp_q.enqueue(q.first());
+            }
+
+            q.dequeue();
+
+            i++;
+        }
+    }
+    while(!temp_q.isEmpty()) {
+        q.enqueue(temp_q.first());
+        temp_q.dequeue();
+    }
+}
+
+int main(){
+  Queue<int>q;
+  q.enqueue(3);
+  q.enqueue(15);
+  q.enqueue(2);
+  q.enqueue(4);
+  sortQueue(q);
+  q.print();
+}
